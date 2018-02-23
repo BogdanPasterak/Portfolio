4abc3e621e54381bc5d542af6cdffaabd6be0ec6
BogdanPasterak <bogdanpasterak@gmail.com>
2018-02-23 16:42:58 +0000

    style: showBP formating

--- showBP.js
+++ showBP.js
@@ -1,19 +1,22 @@
-let ob1;
-let ob2;
 /*
 * showBP
 * author Bogdan Pasterak
 * 21/02/2018
 * my lihgtbox version without jQuery
 * pictures are enough to add showBP class
-* background - hideBP
+* background - hideBP deeper than body
 */
 
+// TODO: shows clicked objects in a possible large format,
+// blurring the background
+
+// on start
 document.addEventListener("DOMContentLoaded", function(event) {
 
-//for each
+  // collection of objects for presentation
   const pic = document.getElementsByClassName("showBP");
 
+  // registration of events for objects (passing this)
   for (let i = 0; i < pic.length; i++) {
     pic[i].onclick = function() {
       show(this);
@@ -22,62 +25,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 });
 
+// after clicking, display the object
 const show = (this_one) => {
+  // constants
   const ground = document.getElementsByClassName("hideBP")[0];
   const body = document.getElementsByTagName("body")[0];
-  const rect = this_one.getBoundingClientRect();
-  const br = body.getBoundingClientRect();
-  const w = window.innerWidth;
-  const h = window.innerHeight;
+  const initialRect = this_one.getBoundingClientRect();
+  const bodyRect = body.getBoundingClientRect();
+  // variable (starting position)
+  let tempX = initialRect.x - bodyRect.x;
+  let tempY = initialRect.y - bodyRect.y;
 
-  const startWidth = rect.width;
-  const startHeight = rect.height;
-  const startX = rect.x - br.x;
-  const startY = rect.y -br.y;
-
-
+  // copy of object
   const clon = this_one.cloneNode(true);
 
-  clon.style.width = startWidth + "px";
-  clon.style.height = startHeight + "px";
+  // setting above the original
+  clon.style.width = initialRect.width + "px";
   clon.style.position = "absolute"
-  clon.style.left = (startX) + "px";
-  clon.style.top = (startY) + "px";
+  clon.style.left = (tempX) + "px";
+  clon.style.top = (tempY) + "px";
 
-  let scale = ((w / rect.width) < (h / rect.height)) ? w / rect.width : h / rect.height;
-  console.log(w + "  " + h + "   sk = " + scale);
+  // calculation of the enlargement
+  let scale = ((window.innerWidth / initialRect.width) < (window.innerHeight / initialRect.height)) ? window.innerWidth / initialRect.width : window.innerHeight / initialRect.height;
+  console.log(window.innerWidth + "  " + window.innerHeight + "   sk = " + scale);
   scale = ((scale > 4) ? 4 : scale) * 0.95;
   scale = (scale > 1.2) ? scale * 0.9 : 1;
-  const endWidth = startWidth * scale;
-  const endHeight = startHeight * scale;
-  const endX = (scale == 1) ? startX : (w - endWidth) / 2;
-  const endY = (h - endHeight) / 2;
 
+  // targets of the object after transformation
+  const endWidth = initialRect.width * scale;
+  const endX = (scale == 1) ? tempX : (window.innerWidth - endWidth) / 2;
+  const endY = (window.innerHeight - initialRect.height * scale) / 2;
+
+  // blurring the background
   ground.style.filter = "blur(4px)";
+  // adding clon
   body.appendChild(clon);
 
+  // transformation counter
   let pos = 1;
 
-  console.log(w + "  " + h + "   sk = " + scale);
-  console.log(startWidth + "  " + startHeight + "  " + startX + "  " + startY);
-  console.log(endWidth + "  " + endHeight + "  " + endX + "  " + endY);
+  tempX -= endX;
+  tempY -= endY;
 
-
+  // transformation loop with interval 0.07 sec.
   const id = setInterval( function() {
     if (pos <= 0) {
       clearInterval(id);
     } else {
       pos -= 0.01;
-      clon.style.top = (endY + (startY - endY) * pos) + 'px';
-      clon.style.left = (endX + (startX - endX) * pos) + 'px';
-      clon.style.width = (endWidth + (startWidth - endWidth) * pos) + 'px';
-      clon.style.height = (endHeight + (startHeight - endHeight) * pos) + 'px';
+      clon.style.top = (endY + tempY * pos) + 'px';
+      clon.style.left = (endX + tempX * pos) + 'px';
+      clon.style.width = (endWidth + (initialRect.width - endWidth) * pos) + 'px';
+      // clon.style.height = (endHeight + (initialRect.height - endHeight) * pos) + 'px';
     }
   }, 7);
-
-
-
-  ob1 = this_one;
-  ob2 = clon;
 
 };
